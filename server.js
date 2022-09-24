df1ff003133c25935d856aec1a1270857df7c4af
LukeSchaffel <lukeschaffel@gmail.com>
2022-09-24 16:06:36 -0400

    auth

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
+require('dotenv').config()
 const express = require('express')
 
 const app = express()
+const jwt = require('jsonwebtoken')
+app.use(express.json())
+const authenticateToken = require('./middleware/auth.js').authenticateToken
+
+const posts = [
+  {
+    username: 'luke',
+    post: 'hellow, me, please allow me to introuce me to me =)'
+  },
+  {
+    username: 'ringo',
+    post: 'awooooooffffffffffff '
+  }
+]
 
 
+app.post('/login', (req, res) => {
 
-app.get('/posts', (req, res)=> {
-  res.send({myAss: "reeeeee"})
+  const username = req.body.username
+  const user = { name: username }
+  
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+  res.json({ accessToken: accessToken })
+
 })
 
-
+app.get('/posts', authenticateToken, (req, res) => {
+  res.json(posts.filter(post => post.username === req.user.name))
+})
 
 
 
